f388f55d56e4984153fa7604b4d346635afdf958
Ori Gritzman <ori.gritzman@gmail.com>
2026-03-17 14:50:06 +0200

    conflict module updated

--- app/server/src/modules/conflict/conflict.module.ts
+++ app/server/src/modules/conflict/conflict.module.ts
@@ -6,11 +6,12 @@ import { ConflictService } from './conflict.service';
 import { ConflictResolverService } from './conflict-resolver.service';
 import { ConflictController } from './conflict.controller';
 import { EntityServiceRegistryModule } from '../../shared/modules/entity-service-registry.module';
+import { RevertService } from './revert.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ConflictEntity]), EntityServiceRegistryModule],
   controllers: [ConflictController],
-  providers: [ConflictRepository, ConflictService, ConflictResolverService],
+  providers: [ConflictRepository, ConflictService, ConflictResolverService, RevertService],
   exports: [ConflictRepository, ConflictService],
 })
 export class ConflictModule {}
