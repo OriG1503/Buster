978672bd19ab24dbfd6ac5f9934ee26851ca0fe7
Ori Gritzman <ori.gritzman@gmail.com>
2026-03-19 22:23:36 +0200

    separated type to a proper file

--- app/client/src/app/core/services/table-view.service.ts
+++ app/client/src/app/core/services/table-view.service.ts
@@ -2,15 +2,8 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { TableViewQuery } from '../../shared/types/table-view-query.type';
 import { TableViewResponse } from '../../shared/types/table-view-response.type';
-
-type TableViewQuery = {
-  tableName: string;
-  columns: string[];
-  filters: Record<string, string>;
-  page: number;
-  pageSize: number;
-};
 
 @Injectable({ providedIn: 'root' })
 export class TableViewService {
