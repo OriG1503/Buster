1ed375133086f84c51ab1478ed031b5dfd76692c
Ori Gritzman <ori.gritzman@gmail.com>
2026-03-20 17:34:54 +0200

    changed parser port

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ echo Starting all Buster services...
 
 start "Client"  cmd /k "cd /d %~dp0app\client  && npm start"
 start "Server"  cmd /k "cd /d %~dp0app\server  && npm run start:dev"
-start "Parser"  cmd /k "cd /d %~dp0app\parser  && uvicorn main:app --reload"
+start "Parser"  cmd /k "cd /d %~dp0app\parser  && uvicorn main:app --reload --port 8999"
 start "Files"   cmd /k "cd /d %~dp0app\files   && npm start"
 
 echo All 4 services launched.
