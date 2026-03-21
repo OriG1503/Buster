77dc01cbffbc46299b2a7c0fadc9aa5eed558c22
unknown <merdok226@gmail.com>
2026-03-21 20:13:31 +0200

    export excel only whats loaded

--- app/client/src/app/core/layouts/home-layout/home-layout.component.ts
+++ app/client/src/app/core/layouts/home-layout/home-layout.component.ts
@@ -1,8 +1,11 @@
 import { Component, computed, effect, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import * as XLSX from 'xlsx';
 
 import { HomeTableComponent } from '../../../features/home/organisms/home-table/home-table.component';
 import { TableActionBarComponent } from '../../../features/home/organisms/table-action-bar/table-action-bar.component';
+import { FK_TO_ENTITY_ID } from '../../../shared/consts/fk-to-entity-id.consts';
+import { ENTITY_COLUMN_LABEL_MAP } from '../../../shared/mapping/entity-column.label-map';
 import { ColumnToggleEvent } from '../../../shared/types/column-toggle-event.type';
 import { HomeStore } from '../../store/home.store';
 import { UploadDialogService } from '../../services/upload-dialog/upload-dialog.service';
@@ -54,5 +57,20 @@ export class HomeLayoutComponent {
     this._uploadDialogService.open();
   }
 
-  public onExportExcel(): void {}
+  public onExportExcel(): void {
+    const cols = this._store.selectedColumns();
+    const displayCols = cols.filter((col) => {
+      if (!col.endsWith('.id')) { return true; }
+      const fkKey = FK_TO_ENTITY_ID[col];
+      return !(fkKey && cols.includes(fkKey));
+    });
+
+    const headers = displayCols.map((col) => ENTITY_COLUMN_LABEL_MAP[col] ?? col);
+    const dataRows = this._store.rows().map((row) => displayCols.map((col) => row[col]?.value ?? ''));
+
+    const ws = XLSX.utils.aoa_to_sheet([headers, ...dataRows]);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, this._store.selectedTable());
+    XLSX.writeFile(wb, `${this._store.selectedTable()}.xlsx`);
+  }
 }
